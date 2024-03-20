f0c31c8ee84913a04c9cc472d8404a3fdf49cd02
Cyrus Oriendo <ianbibotjolly@MacBook-Pro-4.fios-router.home>
2024-03-20 19:02:10 -0400

    Integrated Map into website, needs to be formatted properly

---
+++ client/anime-destruction/src/app/components/Map.tsx
@@ -0,0 +1,53 @@
+'use client' //might limit the changing of choose event but we'll see
+
+
+import React, { useEffect } from 'react';
+import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
+import L from 'leaflet'; // Import Leaflet library
+
+const MapComponent: React.FC = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+        script.integrity = 'sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=';
+        script.crossOrigin = '';
+
+        script.onload = () => {
+            // Leaflet script is loaded, initialize the map
+            const map = L.map('map').setView([51.505, -0.09], 13);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+        };
+
+        document.head.appendChild(script);
+
+        // Cleanup: remove the script element when the component unmounts
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div>
+            <link
+                rel="stylesheet"
+                href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+                integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+                crossOrigin=""
+            />
+            <div>
+                <style>
+                    {`
+                    #map { height: 350px; }
+                    `}
+                </style>
+                <div id="map"></div>
+            </div>
+        </div>
+    );
+}
+
+export default MapComponent;
